57b5f42d2d0b9a15862dd92221827406f4fb487d
pradip bhatt <84025490+pradipbhatt@users.noreply.github.com>
2022-12-31 22:44:39 +0545

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("#todo-form");
 const input = document.querySelector("#todo-input");
 const list = document.querySelector("#todo-list");
+const floatingActionBtn = document.querySelector(".floating-action-btn");
 
 // Add a new task to the list
 form.addEventListener("submit", event => {
@@ -21,6 +22,24 @@ list.addEventListener("change", event => {
   if (event.target.tagName === "INPUT") {
     updateTask(event.target.parentNode);
   }
+});
+
+// Show the floating action button when the list has tasks
+list.addEventListener("click", event => {
+  const tasks = list.querySelectorAll("li");
+  if (tasks.length > 0) {
+    floatingActionBtn.classList.remove("hidden");
+  }
+});
+
+// Hide the floating action button when the list is empty
+floatingActionBtn.addEventListener("click", () => {
+  floatingActionBtn.classList.add("hidden");
+});
+
+// Share the list
+floatingActionBtn.addEventListener("click", () => {
+  shareList();
 });
 
 // Save the list to local storage
@@ -54,32 +73,48 @@ function updateTask(task) {
   const checkbox = task.querySelector("input[type='checkbox']");
   const span = task.querySelector("span");
   span.style.textDecoration = checkbox.checked ? "line-through" : "none";
-  span.style.color = checkbox.checked ? "#ccc" : "#000";
+  span.style.color = checkbox.checked ? "#ccc" : "#222";
+  task.classList.toggle("completed", checkbox.checked);
 }
 
-// Save the list to local storage
+
+
+    // Save the list to local storage
 function saveList() {
+    const tasks = list.querySelectorAll("li");
+    const data = [];
+    tasks.forEach(task => {
+      const checkbox = task.querySelector("input[type='checkbox']");
+      const text = task.querySelector("span").textContent;
+      data.push({
+        text: text,
+        completed: checkbox.checked
+      });
+    });
+    localStorage.setItem("todo-list", JSON.stringify(data));
+  }
+  
+
+  const shareBtn = document.querySelector("#share-btn");
+
+// Share the list when the share button is clicked
+shareBtn.addEventListener("click", () => {
+  shareList();
+});
+
+// Share the list
+function shareList() {
   const tasks = list.querySelectorAll("li");
-  const listArray = [];
+  const data = [];
   tasks.forEach(task => {
     const checkbox = task.querySelector("input[type='checkbox']");
     const text = task.querySelector("span").textContent;
-    const completed = checkbox.checked;
-    listArray.push({ text, completed });
-  });
-  localStorage.setItem("todoList", JSON.stringify(listArray));
-}
-
-// Load the list from local storage
-function loadList() {
-  const listArray = JSON.parse(localStorage.getItem("todoList"));
-  if (listArray) {
-    listArray.forEach(task => {
-      addTask(task.text);
-      const li = list.querySelector("li:last-child");
-      const checkbox = li.querySelector("input[type='checkbox']");
-      checkbox.checked = task.completed;
-      updateTask(li);
+    data.push({
+      text: text,
+      completed: checkbox.checked
     });
-  }
+  });
+  const url = `https://todo-list.com/share?data=${encodeURIComponent(JSON.stringify(data))}`;
+  navigator.clipboard.writeText(url);
+  alert("The list has been shared! The URL has been copied to your clipboard.");
 }
